b4e77ba948870e7341a00b862d16a29cb0ed4021
Joakim Nystrom <jnystromdesign@gmail.com>
2016-06-10 11:05:58 +0200

    Shop front is called with PLL instead of $polylang
    Polylang 1.8 throws a warning if we are using the old syntax.

--- src/Hyyan/WPI/Pages.php
+++ src/Hyyan/WPI/Pages.php
@@ -88,9 +88,8 @@ class Pages
         $shopID = wc_get_page_id('shop');
         $shopOnFront = ('page' === get_option('show_on_front')) && in_array(
                         get_option('page_on_front')
-                        , $polylang->model->get_translations(
-                                'post', $shopID
-        ));
+                        , PLL()->model->post->get_translations($shopID)
+                        );
 
         $vars = array('pagename', 'page', 'name');
         foreach ($vars as $var) {
